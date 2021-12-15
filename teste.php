068ab12788054b45bb21c077b2bc4b32583e7d03
guillermedev75 <gui.manhaes75@gmail.com>
2021-12-15 14:56:15 +0000

    teste

--- teste.php
+++ teste.php
@@ -2,8 +2,8 @@
 
 require 'conexao.php';
 
-$res = $connection->query("SET @@auto_increment_increment=1");
-$res = $connection->query("SET @@auto_increment_offset=1");
+$sql = $connection->query("SET @@auto_increment_increment=1; SET @@auto_increment_offset=1");
+
 
 var_dump($res->fetchAll(PDO::FETCH_ASSOC));
 
