d43fc793d1ceb11c7f51581078980ebe1f100a5e
guillermedev75 <gui.manhaes75@gmail.com>
2021-12-15 00:14:54 -0300

    conexao nova

--- conexao.php
+++ conexao.php
@@ -5,10 +5,10 @@
     $tableClientes = "clientes";
 
 
-    $user   = 'root';
-    $pw     = '';
-    $dbname = 'oil_sistem';
-    $host   = 'localhost';
+    $user   = 'b2a1a08b3cfae5';
+    $pw     = '4ddf4ad1';
+    $dbname = 'heroku_eba83246de940b2';
+    $host   = 'us-cdbr-east-05.cleardb.net';
 
     $connectionString = "mysql:host=".$host.";dbname=".$dbname.",".$user.",".$pw;
 
