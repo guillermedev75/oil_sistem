3dc50ca6cb828cdd6263c573aabe04810e50ba03
guillermedev75 <gui.manhaes75@gmail.com>
2021-12-15 14:54:35 +0000

    teste

--- teste.php
+++ teste.php
@@ -2,7 +2,8 @@
 
 require 'conexao.php';
 
-$res = $connection->query("SHOW VARIABLES LIKE 'auto_inc%'");
+$res = $connection->query("SET @@auto_increment_increment=1");
+$res = $connection->query("SET @@auto_increment_offset=1");
 
 var_dump($res->fetchAll(PDO::FETCH_ASSOC));
 
